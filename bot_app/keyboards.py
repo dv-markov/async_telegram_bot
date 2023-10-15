2f6c127bc26d5383b68d0edc1a1f4a6ea74110ef
dv-markov <dmitry.v.markov@gmail.com>
2023-10-15 14:28:22 +0300

    add https to keyboard

--- bot_app/keyboards.py
+++ bot_app/keyboards.py
@@ -5,7 +5,7 @@ from aiogram.types import (
     InlineKeyboardMarkup,
 )
 
-inline_button_site = InlineKeyboardButton(text="Перейти на сайт", url='http://realworker.ru')
+inline_button_site = InlineKeyboardButton(text="Перейти на сайт", url='https://realworker.ru')
 inline_kb = InlineKeyboardMarkup(inline_keyboard=[[inline_button_site]])
 # inline_kb.add(inline_button_site)
 
