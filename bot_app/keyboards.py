de15e0c31ef3126c739693df76dea108c22d2956
dv-markov <dmitry.v.markov@gmail.com>
2023-10-22 22:56:04 +0300

    remove obsolete keyboards

--- bot_app/keyboards.py
+++ bot_app/keyboards.py
@@ -5,10 +5,10 @@ from aiogram.types import (
     InlineKeyboardMarkup,
 )
 
-inline_button_site = InlineKeyboardButton(text="Перейти на сайт",
-                                          url='https://realworker.ru',
-                                          )
-inline_kb_site = InlineKeyboardMarkup(inline_keyboard=[[inline_button_site]])
+# inline_button_site = InlineKeyboardButton(text="Перейти на сайт",
+#                                           url='https://realworker.ru',
+#                                           )
+# inline_kb_site = InlineKeyboardMarkup(inline_keyboard=[[inline_button_site]])
 # inline_kb.add(inline_button_site)
 
 contact_button = KeyboardButton(text='Отправить контакт',
