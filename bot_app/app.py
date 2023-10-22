8e258995b02b3b16dde6f4823098817eea8fb7d9
dv-markov <dmitry.v.markov@gmail.com>
2023-10-22 22:54:50 +0300

    update to run in new system - phone verification

--- bot_app/app.py
+++ bot_app/app.py
@@ -2,7 +2,7 @@ import asyncio
 import logging
 import sys
 
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, types, F
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message
@@ -27,49 +27,104 @@ async def command_start_handler(message: Message) -> None:
     """
     This handler receives messages with `/start` command
     """
-    await message.answer(f"""Добро пожаловать, {hbold(message.from_user.full_name)}!
-Введите код подтверждения, полученный на сайте realworker.ru для завершения регистрации""")
+    await message.answer(
+        f"""Добро пожаловать, {hbold(message.from_user.full_name)}!
+Вас приветствует телеграм-бот сайта realworker.ru 
+Для завершения регистрации подтвердите свой телефон нажатием кнопки "Отправить контакт" ниже""",
+        reply_markup=keyboards.contact_kb,
+    )
 
 
-async def submit_verification_code(code, user_name, tg_id):
+# async def submit_verification_code(code, user_name, tg_id):
+#     ssl_context = ssl.create_default_context(cafile=certifi.where())
+#     async with aiohttp.ClientSession() as session:
+#         request_data = {
+#             'code': code,
+#             'telegram_id': tg_id,
+#             'telegram_name': user_name,
+#             'tg-bot-token': local_settings.DJANGO_ACCESS_KEY
+#         }
+#         async with session.post(
+#                 local_settings.VERIFY_CODE_URL,
+#                 data=request_data,
+#                 ssl=ssl_context,
+#         ) as response:
+#             return await response.json()
+
+
+async def submit_phone(phone, user_name, tg_id):
     ssl_context = ssl.create_default_context(cafile=certifi.where())
     async with aiohttp.ClientSession() as session:
         request_data = {
-            'code': code,
-            'telegram_id': tg_id,
-            'telegram_name': user_name,
-            'tg-bot-token': local_settings.DJANGO_ACCESS_KEY
+            'phone': phone,
+            'tg_id': tg_id,
+            'tg_name': user_name,
+            'tg_bot_token': local_settings.DJANGO_ACCESS_KEY
         }
-        async with session.post(
-                local_settings.VERIFY_CODE_URL,
+        async with session.patch(
+                local_settings.VERIFY_PHONE_URL,
                 data=request_data,
                 ssl=ssl_context,
         ) as response:
-            return await response.json()
+            return await response.json(content_type='application/json')
+
+
+@dp.message(F.contact)
+async def contact_handler(message: types.Message) -> None:
+    verified = None
+    try:
+        phone = message.contact.phone_number
+        user_name = message.from_user.full_name
+        tg_id = message.from_user.id
+        res = await submit_phone(phone, user_name, tg_id)
+        verified = res.get('verified')
+    except Exception as e:
+        msg = f"Ошибка верификации, {e}"
+        await message.answer(msg)
+
+    if verified:
+        msg = 'Телефон подтвержден!'
+        await message.answer(msg, reply_markup=types.ReplyKeyboardRemove())
+    else:
+        msg = 'Телефон не подтвержден, пройдите регистрацию на сайте realworker.ru'
+        await message.answer(msg)
 
 
 @dp.message()
 async def echo_handler(message: types.Message) -> None:
-    text = message.text
-    if len(text) == 6 and text.isnumeric():
-        user_name = message.from_user.full_name
-        tg_id = message.from_user.id
-        res = await submit_verification_code(str(text), user_name, tg_id)
-        auth = res.get('authorized')
-        status = res.get('status')
-
-        if auth:
-            msg = 'Код подтвержден! '
-            await message.answer(''.join((msg, status)), reply_markup=keyboards.inline_kb)
-        else:
-            msg = ''
-            await message.answer(''.join((msg, status)))
-
-    else:
-        try:
-            await message.reply("Команда не найдена")
-        except TypeError:
-            await message.reply("Неподдерживаемый тип данных")
+    # text = message.text
+    # if len(text) == 6 and text.isnumeric():
+    #     user_name = message.from_user.full_name
+    #     tg_id = message.from_user.id
+    #     res = await submit_verification_code(str(text), user_name, tg_id)
+    #     auth = res.get('authorized')
+    #     status = res.get('status')
+    #
+    #     if auth:
+    #         msg = 'Код подтвержден! '
+    #         await message.answer(''.join((msg, status)), reply_markup=keyboards.inline_kb_site)
+    #     else:
+    #         msg = ''
+    #         await message.answer(''.join((msg, status)))
+    #
+    # if len(text) == 11 and text.isnumeric():
+    #     user_name = message.from_user.full_name
+    #     tg_id = message.from_user.id
+    #     res = await submit_phone(str(text), user_name, tg_id)
+    #     print(res)
+    #     verified = res.get('verified')
+    #
+    #     if verified:
+    #         msg = 'Телефон подтвержден!'
+    #         await message.answer(msg, reply_markup=types.ReplyKeyboardRemove())
+    #     else:
+    #         msg = 'Телефон не подтвержден, пройдите регистрацию на сайте'
+    #         await message.answer(msg, reply_markup=keyboards.inline_kb_site)
+    # else:
+    try:
+        await message.reply("Команда не найдена")
+    except TypeError:
+        await message.reply("Неподдерживаемый тип данных")
 
 
 async def main() -> None:
--- bot_app/keyboards.py
+++ bot_app/keyboards.py
@@ -5,7 +5,18 @@ from aiogram.types import (
     InlineKeyboardMarkup,
 )
 
-inline_button_site = InlineKeyboardButton(text="Перейти на сайт", url='https://realworker.ru')
-inline_kb = InlineKeyboardMarkup(inline_keyboard=[[inline_button_site]])
+inline_button_site = InlineKeyboardButton(text="Перейти на сайт",
+                                          url='https://realworker.ru',
+                                          )
+inline_kb_site = InlineKeyboardMarkup(inline_keyboard=[[inline_button_site]])
 # inline_kb.add(inline_button_site)
+
+contact_button = KeyboardButton(text='Отправить контакт',
+                                request_contact=True,
+                                )
+contact_kb = ReplyKeyboardMarkup(keyboard=[[contact_button]],
+                                 resize_keyboard=True,
+                                 # one_time_keyboard=True,
+                                 )
+
 
